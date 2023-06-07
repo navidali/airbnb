bbb63f897fa28f3f30d7b11de4b3335887999887
Navid <navidnali@gmail.com>
2023-06-07 01:40:58 -0400

    fixed bug preveting image upload

--- app/components/inputs/ImageUpload.tsx
+++ app/components/inputs/ImageUpload.tsx
@@ -9,7 +9,7 @@ declare global {
     var cloudinary: any
 }
 
-const uploadPreset = "pgc9ehd5";
+const uploadPreset = "epgpo8so";
 
 interface ImageUploadProps {
     onChange: (value: string) => void;
@@ -36,23 +36,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
                 return (
                     <div
                         onClick={() => open?.()}
-                        className="
-              relative
-              cursor-pointer
-              hover:opacity-70
-              transition
-              border-dashed 
-              border-2 
-              p-20 
-              border-neutral-300
-              flex
-              flex-col
-              justify-center
-              items-center
-              gap-4
-              text-neutral-600
-            "
-                    >
+                        className="relative cursor-pointer hover:opacity-70 transition border-dashed border-2 p-20 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600">
                         <TbPhotoPlus
                             size={50}
                         />
@@ -60,8 +44,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
                             Click to upload
                         </div>
                         {value && (
-                            <div className="
-              absolute inset-0 w-full h-full">
+                            <div className="absolute inset-0 w-full h-full">
                                 <Image
                                     fill
                                     style={{ objectFit: 'cover' }}
